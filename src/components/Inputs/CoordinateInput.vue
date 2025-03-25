d09f48726665642e0d7dc584acee9aebf8490f95
TeoBB <76711087+McNobby@users.noreply.github.com>
2025-03-25 18:11:06 +0100

    Fixed reactivity bugs

--- src/components/Inputs/CoordinateInput.vue
+++ src/components/Inputs/CoordinateInput.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import CommonCard from '../CommonCard.vue'
-import { type Ref } from 'vue'
 import NumberInput from './NumberInput.vue'
 import type iCoordinate from '@/interfaces/coordinate'
 
@@ -8,7 +7,7 @@ const { dimension } = defineProps<{
   dimension: string
 }>()
 
-const coords = defineModel<Ref<iCoordinate>>({
+const coords = defineModel<iCoordinate>({
   required: true,
 })
 </script>
@@ -16,8 +15,8 @@ const coords = defineModel<Ref<iCoordinate>>({
 <template>
   <CommonCard class="flex flex-col items-center content-center gap-3">
     <h3>{{ dimension }}</h3>
-    <NumberInput v-model.number="coords.value.x" label="X:" />
-    <NumberInput v-model.number="coords.value.y" label="Y:" />
-    <NumberInput v-model.number="coords.value.z" label="Z:" />
+    <NumberInput v-model.number="coords.x" label="X:" />
+    <NumberInput v-model.number="coords.y" label="Y:" />
+    <NumberInput v-model.number="coords.z" label="Z:" />
   </CommonCard>
 </template>
